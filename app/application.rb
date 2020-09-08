285b5a45881c4304b24b5cea2b92afa735ea1198
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-08 02:11:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
     now = Time.now
     
     if now.hour >=12
-      resp.write "Good Morning!"
-    else
       resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
     end
  
     resp.finish
