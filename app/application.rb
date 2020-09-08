b3e1e97ea9024aec17875a075fd7cc225f728279
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-08 02:10:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,9 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
- 
-    # num_1 = Kernel.rand(1..20)
-    # num_2 = Kernel.rand(1..20)
-    # num_3 = Kernel.rand(1..20)
- 
-    # if num_1==num_2 && num_2==num_3
+    now = Time.now
+    
+    if now.hour >=12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
